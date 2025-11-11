d8637bb64fc759288c6f12c07be60ba9bfa1a6e9
tfq26 <taufeeq_26@outlook.com>
2025-11-10 18:25:32 -0600

    init commit

---
+++ Pegasus.Web/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Pegasus.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pegasus.Web placeholder entry point.");
+        }
+    }
+}
